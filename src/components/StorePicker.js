8da972f3f1af2d418699b02e9cd68f5da4459de7
nesa <nesa@heyyou.com.au>
2017-05-24 12:50:49 +1000

    reference to input element/making  bound to main compo

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,25 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 
+
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('you changed the URL');
+        console.log(this.storeInput.value); //this `this` needs to be bound to the main component, otherwise it's null
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}
+                ref={(input) => {this.storeInput = input}}/>
                 <button type="submit">Visit Store -></button>
             </form>
         ) 
