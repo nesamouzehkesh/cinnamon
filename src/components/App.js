c6b215a1010a62ceacb8974cba97848fe8deba9b
nesamouzehkesh <n.mouzehkesh@gmail.com>
2017-07-18 11:58:03 +1000

    store order state in localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -27,10 +27,24 @@ class App extends React.Component {
             context: this,
             state: 'fishes'
         });
+        
+        // check if there is any order in localStorage
+        const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+        if(localStorageRef) {
+            // update our App component's order state
+            this.setState({
+                order: JSON.parse(localStorageRef)
+            });
+        }
     }
 
     componentWillUnmount() {
         base.removeBinding(this.ref);
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
     }
 
     
@@ -72,7 +86,10 @@ class App extends React.Component {
                         }
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Order fishes={this.state.fishes}
+                       order={this.state.order}
+                       params={this.props.params}
+                />
                 <Inventory addFish={this.addFish} loadFishes={this.loadFishes} />
             </div>
         )
